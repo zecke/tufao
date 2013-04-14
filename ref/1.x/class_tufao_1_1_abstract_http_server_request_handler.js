a09a6b325ab22428b77136afb58524ffac7ec142
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2013-04-13 21:28:56 -0300

    Added Tufão documentation to version 1.0

---
+++ ref/1.x/class_tufao_1_1_abstract_http_server_request_handler.js
@@ -0,0 +1,6 @@
+var class_tufao_1_1_abstract_http_server_request_handler =
+[
+    [ "~AbstractHttpServerRequestHandler", "class_tufao_1_1_abstract_http_server_request_handler.html#aa3d042121a8664b6448e19bfb0a1f1aa", null ],
+    [ "handleRequest", "class_tufao_1_1_abstract_http_server_request_handler.html#aa0391acca2efe108644c9e7bc3575af8", null ],
+    [ "operator std::function< bool", "class_tufao_1_1_abstract_http_server_request_handler.html#aa1cc8e7e0e5c7f4c91eeed8542004177", null ]
+];
